5dfcca95bfb35afbe779497ed063efac030f2c14
frba <frba@cin.ufpe.br>
2019-05-21 15:27:45 -0400

    change input file for combinatorial script

--- libs/function/combinatorial.py
+++ libs/function/combinatorial.py
@@ -1,30 +1,57 @@
 from ..misc import file, calc
-import itertools
+import itertools, csv
 
 
-def get_sets_in_filepath(reader):
-    lists_parts = []
-    lists_combination_parts = []
-    ''' For each line in file get the list'''
-    for line in reader:
-        parts = []
-        sets = line.strip('\n').split(';')
-        '''List of parts'''
-        for set in sets:
-            parts.append(set.split(','))
-        ''' Create the single list of parts'''
-        lists_parts.append(list(parts))
-        ''' Create the combinations from the list'''
-        lists_combination_parts.append(list(itertools.product(*parts)))
+def get_combinations(parts):
+    ''' Create the single list of parts'''
+    lists_parts = list(parts)
+
+    ''' Create the combinations from the list'''
+    lists_combination_parts = list(itertools.product(*parts))
     return lists_combination_parts, lists_parts
 
 
+def get_sets(parts_dct, header):
+    parts = []
+    for i in range(1, len(header)):
+        set = []
+        for row in parts_dct:
+            if row[header[i]] != '':
+                set.append(row[header[i]])
+        if len(set) > 0:
+            parts.append(list(set))
+    return parts
+
+
 def run_combination(path, filename):
+    lists_parts = []
+    lists_combination_parts = []
+
     filein = file.verify(path + "/" + filename)
+    with open(filein.name, 'r') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        header = reader.fieldnames
+        exp_id = -1
+        parts_dct = []
 
-    """Create combinations"""
-    lists_combination_parts, lists_parts = get_sets_in_filepath(filein)
+        for dct in map(dict, reader):
+            if exp_id == -1: exp_id = dct[header[0]]
+            if dct[header[0]] == exp_id:
+                parts_dct.append(dct)
+            else:
+                exp_id = dct[header[0]]
+                parts = get_sets(parts_dct, header)
+                l_combination_parts, l_parts = get_combinations(parts)
+                lists_combination_parts.append(list(l_combination_parts))
+                lists_parts.append(list(l_parts))
+                parts_dct = []
+                parts_dct.append(dct)
 
+        parts = get_sets(parts_dct, header)
+        l_combination_parts, l_parts = get_combinations(parts)
+        lists_combination_parts.append(list(l_combination_parts))
+        lists_parts.append(list(l_parts))
+    
     """Calculate the num of parts in input file"""
     list_set_num_parts = calc.num_listsparts(lists_parts)
 
@@ -37,4 +64,62 @@ def run_combination(path, filename):
     fileout.close()
 
     return fileout, list_set_num_parts, num_combinations_in_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # for dct in map(dict, reader):
+        #     exp_id = dct[header[0]]
+        #     parts = []
+        #
+        #
+        #
+        #
+        #     if firstline:
+        #         firstline = False
+        #         exp_id = dct[header[0]]
+        #         for i in range(1, len(dct)):
+        #             if dct[header[i]] != '':
+        #                 parts.append(dct[header[i]])
+        #         print(parts)
+        #
+        #     if exp_id == dct[header[0]]:
+        #         print(dct[header[0]])
+        #
+        #     else:
+        #         exp_id = dct[header[0]]
+        #         print(dct[header[0]])
+
+
+
+
+    # """Create combinations"""
+    # lists_combination_parts, lists_parts = get_sets_in_filepath(filein)
+    #
+    # """Calculate the num of parts in input file"""
+    # list_set_num_parts = calc.num_listsparts(lists_parts)
+    #
+    # """Calculate number of combinations"""
+    # num_combinations_in_list = calc.num_combinations(lists_combination_parts)
+    #
+    # """Write a output file"""
+    # fileout = file.create(path+"/"+'combination_' + str(filename), 'w')
+    # file.write_combinations(fileout, lists_combination_parts)
+    # fileout.close()
+    #
+    # return fileout, list_set_num_parts, num_combinations_in_list
+    return None, None, None
 
