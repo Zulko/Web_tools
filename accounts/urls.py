5055f88798883c6008c1091761ce9709223a48e6
frba <frba@cin.ufpe.br>
2019-06-18 11:07:52 -0400

    reset page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,8 @@
 from . import views
 from django.urls import path, reverse_lazy
-from django.contrib.auth.views import (
-    PasswordResetView,
-    PasswordResetDoneView,
-    PasswordResetConfirmView,
-    PasswordResetCompleteView
-)
+from django.contrib.auth import views as auth_views
 
-app_name = 'accounts'
+# app_name = 'accounts'
 
 urlpatterns = [
 
@@ -20,24 +15,24 @@ urlpatterns = [
 
     path('profile/password/', views.password_view, name='change_password'),
 
-    # path('password-reset/',
-    #      PasswordResetView.as_view(template_name='registration/password_reset_form.html',
-    #                                email_template_name="registration/password_reset_email.html",
-    #                                success_url=reverse_lazy('accounts:password_reset_done'),
-    #                                ), name='password_reset'),
-    #
-    # path('password-reset/done/',
-    #      PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
-    #      name='password_reset_done'),
-    #
-    # path('password-reset-confirm/<uidb64>/<token>/',
-    #      PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html',
-    #                                       success_url=reverse_lazy('accounts:password_reset_complete')),
-    #      name='password_reset_confirm'),
-    #
-    # path('password-reset-complete/',
-    #      PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
-    #      name='password_reset_complete'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html',
+        email_template_name="registration/password_reset_email.html",
+        success_url=reverse_lazy('password_reset_done'),
+    ), name='password_reset'),
+
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html',
+        success_url=reverse_lazy('password_reset_complete')),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'),
+         name='password_reset_complete'),
 
 
 ]
