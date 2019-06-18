2370689761d2cbbcf936455007995f06065561e1
frba <frba@cin.ufpe.br>
2019-06-18 10:30:51 -0400

    reset page

--- Web_tools/settings.py
+++ Web_tools/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_unused_media',
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'db.apps.DbConfig',
     'misc',
     'scripts',
