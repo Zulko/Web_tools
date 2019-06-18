1990a3277a01d8aa498adc6e1b05f2e3e1ebd3f3
frba <frba@cin.ufpe.br>
2019-06-18 12:11:35 -0400

    reset page

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,24 +15,24 @@ urlpatterns = [
 
     path('profile/password/', views.password_view, name='change_password'),
 
-    # path('password-reset/', auth_views.PasswordResetView.as_view(
-    #     template_name='registration/password_reset_form.html',
-    #     email_template_name="registration/password_reset_email.html",
-    #     success_url=reverse_lazy('password_reset_done'),
-    # ), name='password_reset'),
-    #
-    # path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
-    #     template_name='registration/password_reset_done.html'),
-    #      name='password_reset_done'),
-    #
-    # path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-    #     template_name='registration/password_reset_confirm.html',
-    #     success_url=reverse_lazy('password_reset_complete')),
-    #      name='password_reset_confirm'),
-    #
-    # path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
-    #     template_name='registration/password_reset_complete.html'),
-    #      name='password_reset_complete'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(
+        template_name='../templates/registration/password_reset_form.html',
+        email_template_name="../templates/registration/password_reset_email.html",
+        success_url=reverse_lazy('password_reset_done'),
+    ), name='password_reset'),
+
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='../templates/registration/password_reset_done.html'),
+         name='password_reset_done'),
+
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='../templates/registration/password_reset_confirm.html',
+        success_url=reverse_lazy('password_reset_complete')),
+         name='password_reset_confirm'),
+
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='../templates/registration/password_reset_complete.html'),
+         name='password_reset_complete'),
 
 
 ]
