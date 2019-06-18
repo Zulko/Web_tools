6eeca69350bfe5ef246bcec7be49264b62a86c15
frba <frba@cin.ufpe.br>
2019-06-18 12:52:18 -0400

    reset page

--- accounts/urls.py
+++ accounts/urls.py
@@ -16,22 +16,22 @@ urlpatterns = [
     path('profile/password/', views.password_view, name='change_password'),
 
     path('password_reset/', auth_views.PasswordResetView.as_view(
-        template_name='../templates/registration/password_reset_form.html',
-        email_template_name="../templates/registration/password_reset_email.html",
+        template_name='accounts/password_reset_form.html',
+        email_template_name="accounts/password_reset_email.html",
         success_url=reverse_lazy('accounts:password_reset_done'),
     ), name='password_reset'),
 
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='../templates/registration/password_reset_done.html'),
+        template_name='accounts/password_reset_done.html'),
          name='password_reset_done'),
 
     path('password_reset_<uidb64>_<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='../templates/registration/password_reset_confirm.html',
+        template_name='accounts/password_reset_confirm.html',
         success_url=reverse_lazy('accounts:password_reset_complete')),
          name='password_reset_confirm'),
 
     path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='../templates/registration/password_reset_complete.html'),
+        template_name='accounts/password_reset_complete.html'),
          name='password_reset_complete'),
 
 
