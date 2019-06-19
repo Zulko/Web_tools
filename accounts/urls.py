674d100d81efdc6a094e2de3a2d85f4c1775ef37
frba <frba@cin.ufpe.br>
2019-06-19 14:13:02 -0400

    reset page

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     # path('logout/',
     #      views.logout_view, name='logout'),
 
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('signup/',
@@ -29,8 +29,8 @@ urlpatterns = [
          views.password_view, name='change_password'),
 
     path('password_reset/',
-         views.PasswordResetView.as_view(template_name='registration/password_change_form.html',
-                                         email_template_name='registration/reset_password_email.html',
+         views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html',
+                                         email_template_name='accounts/reset_password_email.html',
                                          success_url=reverse_lazy('accounts:reset_password_done')),
          name='password_reset'),
 
