f0d1cc060934ff3e21113b6608d71bf60adbe448
frba <frba@cin.ufpe.br>
2019-06-19 10:37:33 -0400

    reset page

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import PasswordResetView
 
 from . import views
 
-
+app_name = 'accounts'
 
 urlpatterns = [
 
@@ -15,6 +15,7 @@ urlpatterns = [
 
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
 
     path('signup/',
          views.SignUp.as_view(), name='signup'),
