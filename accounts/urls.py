f512eba682c84dea17da41266a8fd08023ebc6ac
frba <frba@cin.ufpe.br>
2019-06-19 17:15:24 -0400

    reset page

--- accounts/urls.py
+++ accounts/urls.py
@@ -27,7 +27,9 @@ urlpatterns = [
              template_name='registration/password_reset_done.html'), name='password_reset_done'),
 
     path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name='registration/password_reset_confirm.html',
+             success_url=reverse_lazy('accounts:password_reset_complete')), name='password_reset_confirm'),
 
     path('reset/done/',
          auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
